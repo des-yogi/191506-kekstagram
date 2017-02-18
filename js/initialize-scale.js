ef7e26872c5b04d6777bdd660ef72890258ddd36
Igor Beliakovski <nuevo-design@mail.ru>
2017-02-18 13:15:53 +0200

    Рефакторинг initialize-scale

--- js/initialize-scale.js
+++ js/initialize-scale.js
@@ -2,18 +2,24 @@
 
 window.createScale = (function () {
   return function (element, step, initialScale, scaleCallBack) {
+
+    var scaleValueField = document.querySelector('.upload-resize-controls-value');
+
     element.addEventListener('click', function (e) {
       if (e.target.classList.contains('upload-resize-controls-button-inc') && initialScale < 100) {
         initialScale = initialScale + step;
-        element.children[1].value = initialScale + '%';
+        scaleValueField.value = initialScale + '%';
+        // element.children[1].value = initialScale + '%';
       }
       if (e.target.classList.contains('upload-resize-controls-button-dec') && initialScale > 25) {
         initialScale = initialScale - step;
-        element.children[1].value = initialScale + '%';
+        scaleValueField.value = initialScale + '%';
+        // element.children[1].value = initialScale + '%';
       }
       if (typeof scaleCallBack === 'function') {
         scaleCallBack(initialScale);
       }
     });
+
   };
 })();
