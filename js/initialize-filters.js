5355253e08e445c90d6335d37aa819a5b8379d73
Igor Beliakovski <nuevo-design@mail.ru>
2017-02-21 13:48:45 +0200

    Багфикс

--- js/initialize-filters.js
+++ js/initialize-filters.js
@@ -13,10 +13,12 @@ window.initializeFilters = (function () {
       elementPreview.classList.add(filterName);
       currentFilter = filterName;
       e.target.setAttribute('aria-pressed', 'true');
+      window.dragLevelFilters();
     };
 
     element.addEventListener('click', function (e) {
-      if (e.target.tagName === 'INPUT' || e.target.tagName === 'DIV') {
+      if (e.target.tagName === 'INPUT' || e.target.className === 'upload-filter-level-line'
+        || e.target.className === 'upload-filter-level' || e.target.className === 'upload-filter-level-pin') {
         return;
       }
       var str = 'upload-';
